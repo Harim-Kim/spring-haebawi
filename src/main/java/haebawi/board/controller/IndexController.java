8de7eccb623532ca32eaa53909bdf663bc6ba6f8
yongkal2@yonsei.ac.kr <yongkal2@yonsei.ac.kr>
2024-04-21 13:26:24 +0900

    nav index

--- src/main/java/haebawi/board/controller/IndexController.java
+++ src/main/java/haebawi/board/controller/IndexController.java
@@ -1,15 +1,26 @@
 package haebawi.board.controller;
 
 
+import haebawi.board.domain.entity.User;
+import haebawi.board.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
+    private final UserService userService;
     @GetMapping({"","/"})
-    public String index(){
+    public String index(Model model, Principal principal){
+        if (principal == null){
+            return "index";
+        }
+        User user = userService.getLoginUserByLoginId(principal.getName());
+        model.addAttribute("currentUser", user);
         return "index";
     }
 }
--- src/main/java/haebawi/board/controller/SecurityLoginController.java
+++ src/main/java/haebawi/board/controller/SecurityLoginController.java
@@ -38,7 +38,7 @@ public class SecurityLoginController {
             }
         }
 
-        return "home";
+        return "redirect:/";
     }
 
     @GetMapping("/join")
